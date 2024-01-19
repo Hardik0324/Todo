4b9c2d74258facb3d4e0447d627111374f8f7af9
Hardik <hardiksingh24may@gmail.com>
2024-01-19 23:13:16 +0530

    abcdefgh

--- app/api/schedules/route.ts
+++ app/api/schedules/route.ts
@@ -3,14 +3,6 @@ import { connectToDb } from "@/lib/util";
 import { NextResponse } from "next/server";
 import {type NextRequest } from "next/server";
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb',
-    },
-  },
-};
-
 export const GET = async (req: NextRequest) => {
     try {
         connectToDb();
